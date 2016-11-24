264ba6a3af3ef0e8e6f40a4c69d58ad6920c7402
farojos <farojos@uc.cl>
2016-11-24 12:40:48 -0300

    Primera Aplicacion Android, CAMBIO DE TEXTO

---
+++ MyApplication2/app/src/main/java/com/example/feliperojos/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.feliperojos.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView text = (TextView) findViewById(R.id.text_view_QUEDAN5PERSONAS);
+        EditText edit = (EditText) findViewById(R.id.editText);
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                text.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+}
